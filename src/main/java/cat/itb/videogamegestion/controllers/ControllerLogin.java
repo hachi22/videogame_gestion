10d2752c31f99751037f608dcca8f67475fa8088
hachi22 <adrian.nunyez.7e3@itb.cat>
2021-04-27 12:39:23 +0200

    .

--- src/main/java/cat/itb/videogamegestion/controllers/ControllerLogin.java
+++ src/main/java/cat/itb/videogamegestion/controllers/ControllerLogin.java
@@ -33,11 +33,11 @@ public class ControllerLogin {
     @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("user", new UserObject());
-        return "register";
+        return "registerUser";
     }
 
-    @PostMapping("/register")
-    public String afegirSubmit(@ModelAttribute("user") UserObject userObject){
+    @PostMapping("/registerUser")
+    public String afegirSubmit(@ModelAttribute("UserObject") UserObject userObject){
         userObject.setRol("USER");
         userService.putUser(userObject);
         return "redirect:/";
